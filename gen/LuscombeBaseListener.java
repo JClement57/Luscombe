b9a4bc52fb0e8164527f5924f475c6e487dea257
bromlu <lukewilliambrom@gmail.com>
2019-04-17 21:57:42 -0400

    Started work on listener

---
+++ gen/LuscombeBaseListener.java
@@ -0,0 +1,348 @@
+// Generated from /Users/bromlu/Documents/Projects/Luscombe/Luscombe.g4 by ANTLR 4.7.2
+
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.tree.ErrorNode;
+import org.antlr.v4.runtime.tree.TerminalNode;
+import java.util.HashMap;
+import org.stringtemplate.v4.*;
+/**
+ * This class provides an empty implementation of {@link LuscombeListener},
+ * which can be extended to create a listener which only needs to handle a subset
+ * of the available methods.
+ */
+public class LuscombeBaseListener implements LuscombeListener {
+	int currentLocationIndex = 0;
+	HashMap locationMap = new HashMap();
+	public String program = "";
+	public String location = "";
+	public String currentFunction = "";
+	public String locationStringTemplate = "locations = [\n" +
+			"{\n" +
+			"    <location>\n" +
+			"},\n" +
+			"];";
+//
+//	public String actionsTemplate =
+//			"<object>: [, 1],\n" +
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterProgram(LuscombeParser.ProgramContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitProgram(LuscombeParser.ProgramContext ctx) {
+		ST programTemplate = new ST(program);
+		for (Object key : locationMap.keySet()) {
+			programTemplate.add(key.toString(), locationMap.get(key).toString());
+		}
+		program = programTemplate.render();
+		System.out.println(program);
+	}
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterInventory(LuscombeParser.InventoryContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitInventory(LuscombeParser.InventoryContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterItem(LuscombeParser.ItemContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitItem(LuscombeParser.ItemContext ctx) {
+		currentFunction = "";
+	}
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterUse(LuscombeParser.UseContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitUse(LuscombeParser.UseContext ctx) {
+		currentFunction = "";
+	}
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterLocation(LuscombeParser.LocationContext ctx) {
+		location += "name: '" + ctx.start.getText() + "',\n";
+		locationMap.put(ctx.start.getText().toLowerCase(), currentLocationIndex++);
+	}
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitLocation(LuscombeParser.LocationContext ctx) {
+		program += location;
+		location = "";
+	}
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterProperties(LuscombeParser.PropertiesContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitProperties(LuscombeParser.PropertiesContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterCounters(LuscombeParser.CountersContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitCounters(LuscombeParser.CountersContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterDeclarations(LuscombeParser.DeclarationsContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitDeclarations(LuscombeParser.DeclarationsContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterCommand(LuscombeParser.CommandContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitCommand(LuscombeParser.CommandContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterAdd(LuscombeParser.AddContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitAdd(LuscombeParser.AddContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterDrop(LuscombeParser.DropContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitDrop(LuscombeParser.DropContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterGotolocation(LuscombeParser.GotolocationContext ctx) {
+		currentFunction += "goto(<" + ctx.getChild(LuscombeParser.NameContext.class, 0).getText().toLowerCase() + ">);\n";
+	}
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitGotolocation(LuscombeParser.GotolocationContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterConditional(LuscombeParser.ConditionalContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitConditional(LuscombeParser.ConditionalContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterIfblock(LuscombeParser.IfblockContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitIfblock(LuscombeParser.IfblockContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterElseblock(LuscombeParser.ElseblockContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitElseblock(LuscombeParser.ElseblockContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterComparision(LuscombeParser.ComparisionContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitComparision(LuscombeParser.ComparisionContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterIntro(LuscombeParser.IntroContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitIntro(LuscombeParser.IntroContext ctx) {
+		currentFunction = "";
+	}
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterActions(LuscombeParser.ActionsContext ctx) {
+		location += "actions: {\n";
+	}
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitActions(LuscombeParser.ActionsContext ctx) {
+		location += "},\n";
+	}
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterAction(LuscombeParser.ActionContext ctx) {
+		currentFunction += ": () => {\n";
+	}
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitAction(LuscombeParser.ActionContext ctx) {
+		currentFunction += "},\n";
+		ctx.getChild(LuscombeParser.NameContext.class, 0);
+		for (int i = 0; i < ctx.getChildCount(); i++) {
+			if(ctx.getChild(i).getClass() == LuscombeParser.NameContext.class) {
+				location += ctx.getChild(i).getText() + currentFunction;
+			}
+		}
+		currentFunction = "";
+	}
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterObjects(LuscombeParser.ObjectsContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitObjects(LuscombeParser.ObjectsContext ctx) {
+		currentFunction = "";
+	}
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterName(LuscombeParser.NameContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitName(LuscombeParser.NameContext ctx) { }
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterEveryRule(ParserRuleContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitEveryRule(ParserRuleContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void visitTerminal(TerminalNode node) {
+		if(node.getText().contains("PRINT")) {
+			int start = node.getText().indexOf('{');
+			int end = node.getText().indexOf('}');
+			currentFunction += "print('" + node.getText().substring(start + 1, end).trim() + "');\n";
+		}
+	}
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void visitErrorNode(ErrorNode node) { }
+}
--- src/Luscombe.java
+++ src/Luscombe.java
@@ -1,2 +1,37 @@
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+import java.nio.file.*;
+import org.stringtemplate.v4.*;
+import java.util.*;
+
 public class Luscombe {
+    public static void main(String[] args) throws Exception {
+        CharStream input = CharStreams.fromFileName("exampleInput.txt");
+        LuscombeLexer lexer = new LuscombeLexer(input);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        LuscombeParser parser = new LuscombeParser(tokens);
+        LuscombeParser.ProgramContext prog = parser.program();// parse the input stream!
+
+//        ST hello = new ST("Hello, <name>");
+//        hello.add("name", "World");
+//        System.out.println(hello.render());
+
+//        List<String> template = Files.readAllLines(Paths.get("template.html"));
+//        String templateString = "";
+//        for (String str: template) {
+//            str = str.replaceAll("\\s+","");
+//            templateString += str;
+//        }
+//
+//        ST hello = new ST(templateString);
+//        System.out.println(templateString);
+
+        Path copied = Paths.get("output.html");
+        Path originalPath = Paths.get("template.html");
+        Files.copy(originalPath, copied, StandardCopyOption.REPLACE_EXISTING);
+
+        ParseTreeWalker walker = new ParseTreeWalker();
+        LuscombeListener listener = new LuscombeBaseListener();
+        walker.walk(listener, prog);
+    };
 }
